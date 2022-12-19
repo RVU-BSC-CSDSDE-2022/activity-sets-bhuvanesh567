2d642e32788bc6b1561636f3d713d27ac8ff9b9d
Kamatham Lakshmi Naga Lakshmi <114483191+bhuvanesh567@users.noreply.github.com>
2022-12-19 04:37:45 +0000

    problem solved

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
-  int a, b,sum;
-  a=4 ,b=7;
-  sum=a+b;
-  printf("%d %d %d ", sum ,a , b);
+int main(void){
+  int a, b, sum;
+  a = 10 , b= 20;
+  sum= a+b;
+  printf("%d %d %d \n", sum, a, b);
   return 0;
 }
