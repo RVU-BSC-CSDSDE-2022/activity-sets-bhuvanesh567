0d24778ef0758f01b22fe64617bb31c8aae44e0e
Kamatham Lakshmi Naga Lakshmi <114483191+bhuvanesh567@users.noreply.github.com>
2022-12-19 04:32:03 +0000

    problem solved

---
+++ set01/problem03.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int input();
+int sum_func(int a, int b);
+void output(int a, int b, int sum);
+
+int main(void){
+  int a, b, sum;
+  a =input();
+  b= input();
+  sum=sum_func(a, b);
+  output(a, b, sum);
+  return 0;
+}
+int input(){
+  int a;
+  printf("Enter the number\n");
+  scanf("%d", &a);
+  return a;
+}
+int sum_func(int a, int b){
+  int sum;
+  sum = a + b;
+  return sum;
+}
+void output(int a, int b, int sum){ 
+printf("The sum of %d + %d is %d",sum,a,b);
+}
+  
