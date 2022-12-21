0caff122f155c9b66917fb0879244a980e30d381
Kamatham Lakshmi Naga Lakshmi <114483191+bhuvanesh567@users.noreply.github.com>
2022-12-21 04:13:31 +0000

    problem solved

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,24 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int input();
-void add(int a, int b, int *sum);
+void add (int a, int b, int *sum);
 void output(int a, int b, int sum);
 
 int main(){
-   int x, y, sum;
-  printf("Enter the number");
-  scanf("%d",&x);
-  printf("Enter the number");
-  scanf("%d",&y);
-  sum_func(&x, &y,&sum);
-  printf("%d", sum);
+  int a, b, sum;
+  a = input();
+  b = input();
+  add(a, b, &sum);
+  output(a, b, sum);
   return 0;
 }
-void add(int *x,int *y, int *sum)
-{
-  *sum=*x+*y;
+int input(){
+  int x;
+  printf("Enter the number");
+  scanf("%d", &x);
+  return x;
 }
-void output(int x, int y, int sum){
-  printf(" %d and %d is %d\n",x, y, sum);
+
+void add (int a, int b, int *sum){
+  *sum= a+b;
+}
+void output(int a, int b, int sum){
+  printf("%d of %d is%d", a, b, sum);
 }
 
